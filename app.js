7b78f94817eb56fa94584a30466f46d52a18d5f8
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-04-17 15:49:04 +0530

    hi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
-  res.json({ status: "Server is running" });
+  res.json({ status: "Ok" });
 });
 
 app.listen(3000, () => {
